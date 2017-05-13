178c3ff3a76173a2cc6dd0f5408a8b3af17a676f
mitutee <volman1285@gmail.com>
2017-05-13 07:35:02 +0300

    venv added

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+import json
+import platform
+import logging
+import os
+
+from flask import Flask, jsonify
+from flask import request
+
+import language_processor as lp
+
+script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+rel_path = "config.json"
+abs_file_path = os.path.join(script_dir, rel_path)
+with open(abs_file_path) as json_data_file:
+    CONFIG = dict(json.load(json_data_file))
 
 app = Flask(__name__)
+PORT = 9090
 
 @app.route('/')
 def index():
-    return 'My handsome natural language parser'
+    return '<h1>My handsome natural language parser!</h1>'
+
+@app.route('/api')
+def api():
+    peer = request.args.get('peer')
+    msg = request.args.get('msg')
+    token = request.args.get('token')
+    return '{0} wrote: {1}'.format(peer, msg)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=PORT)
