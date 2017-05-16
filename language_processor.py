7afa6926129891f4bb39473ca12b81d911b0b7d8
mitutee <volman1285@gmail.com>
2017-05-16 08:46:20 +0300

    Pretty answer is gotten

--- language_processor.py
+++ language_processor.py
@@ -1,6 +1,48 @@
 import pymorphy2
 import io
-with io.open('pure_cities.txt','r',encoding='utf8') as f:
-    CITIES = f.read().split()
+import requests
+import datetime
+import json
+
+CITIES_URI = "https://raw.githubusercontent.com/mitutee/woommee_wp/master/pure_cities.txt"
+WOOMMEE_WP_URI = "http://mitutee.tech:8888/api"
+CITIES = requests.get(CITIES_URI).text.lower().split()
+
+
+morph = pymorphy2.MorphAnalyzer()
+
+default = 'Oops v_o__0_V       Something is wrong'
+
 def get_answer(user, question):
-    return question in CITIES
+    try:
+        question_tokens = question.split()
+    except AttributeError as e:
+        return default
+    
+    if 'погода' in morph.normal_forms(question_tokens[0]):
+        word1 = morph.normal_forms(question_tokens[1])[0]
+        if len(question_tokens ) > 2:
+            word2 = morph.normal_forms(question_tokens[2])
+            if word2 in CITIES:
+                weather = getWeather(word2)
+        elif word1 in CITIES:
+            weather = getWeather(word1)
+        else:
+            return "No such city"
+        daily = weather["daily"]
+        date = datetime.datetime.fromtimestamp(int(daily["data"][0]["time"])).strftime('%Y-%m-%d %H:%M:%S')
+        answer = "Погода в {0} на {1} : {2}.".format(weather["_city"], date, daily['summary'])
+        return answer
+    else :
+        return default
+
+
+def getWeather(city):
+    res = requests.get("{}/{}".format(WOOMMEE_WP_URI, city)).text
+    raw = json.loads(res)
+    raw["_city"] = city
+    return raw
+    
+
+
+print(get_answer("asdf",'погода Киев'))
